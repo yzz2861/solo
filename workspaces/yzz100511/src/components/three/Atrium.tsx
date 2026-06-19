a032a9023f9188f80507c76f30db1cd677f85a8c
yzz <yangzhenzhou2861@126.com>
2026-06-19 08:57:56 +0800

    batch commit: yzz100511

--- workspaces/yzz100511/src/components/three/Atrium.tsx
+++ workspaces/yzz100511/src/components/three/Atrium.tsx
@@ -1,4 +1,4 @@
-import { useRef, useMemo } from 'react';
+import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { useMallStore } from '../../store/useMallStore';
